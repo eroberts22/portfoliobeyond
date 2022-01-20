3b7802415c641b616e6fbd1d653a7237c82bb33c
Elizabeth <eroberts22@my.whitworth.edu>
2022-01-20 00:48:41 -0800

    simplify design

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+// set active page, doesnt work
+$(document).ready(function () {
+    $('.navbar-links ul li a').click(function () {
+        //removing the previous selected menu state
+        $('.navbar').find('a.active').removeClass('active');
+        //adding the state for this parent menu
+        $(this).parents("a").addClass('active');
+
+    });
+});
+
+// insert nav.html into each page
+$(function () { $("#navbar-placeholder").load("nav.html"); });
+
+// toggle for dropdown menu
 function myFunc() {
     const toggleButton = document.getElementsByClassName('toggle-button')[0];
     const navbarlinks = document.getElementsByClassName('navbar-links')[0];
