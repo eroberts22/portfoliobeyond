3804604db939ebeb51dc2556ea40aeb1c7feda4a
Elizabeth <eroberts22@my.whitworth.edu>
2022-01-20 00:57:02 -0800

    potential bug fix

--- js/script.js
+++ js/script.js
@@ -20,12 +20,15 @@ function myFunc() {
     const bar1 = document.getElementsByClassName('bar')[1];
     const bar2 = document.getElementsByClassName('bar')[2];
 
-    toggleButton.addEventListener('click', function() {
-    navbarlinks.classList.toggle('active');
-    bar0.classList.toggle('active');
-    bar1.classList.toggle('active');
-    bar2.classList.toggle('active');
-});
+    if (toggleButton) {
+        toggleButton.addEventListener('click', function() {
+        navbarlinks.classList.toggle('active');
+        bar0.classList.toggle('active');
+        bar1.classList.toggle('active');
+        bar2.classList.toggle('active');
+        });
+    }
+ 
 };
 
 window.onload = myFunc;
